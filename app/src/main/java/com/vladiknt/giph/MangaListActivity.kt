a5f78095f2d85614414a5cefa7555b5a1f216e59
DevVladikNT <trycatchernt@gmail.com>
2021-04-10 22:41:04 +0300

    added manga

--- app/src/main/java/com/vladiknt/giph/MangaListActivity.kt
+++ app/src/main/java/com/vladiknt/giph/MangaListActivity.kt
@@ -24,6 +24,13 @@ class MangaListActivity : AppCompatActivity() {
             R.id.misaki_full_color -> manga.putExtra("name", "Misaki Full Color")
             R.id.torokase_orgasm -> manga.putExtra("name", "Torokase Orgasm")
             R.id.my_magical_supplement_upon_this_wonderful_wizard -> manga.putExtra("name", "My Magical Supplement upon this Wonderful Wizard")
+            R.id.a_book_where_miku_has_it_her_way -> manga.putExtra("name", "A Book Where Miku Has It Her Way")
+            R.id.amayakashi_mate_pia -> manga.putExtra("name", "Amayakashi Mate Pia")
+            R.id.azulan_anime_ero_mousou_hon -> manga.putExtra("name", "AzuLan Anime Ero Mousou Hon")
+            R.id.bunshin_shite_hamakaze_to_sanketsu_ecchi -> manga.putExtra("name", "Bunshin Shite Hamakaze to Sanketsu Ecchi")
+            R.id.introverted_beauty_gets_raped_over_and_over_by_her_homeroom_teacher -> manga.putExtra("name", "Introverted Beauty Gets Raped Over and Over by Her Homeroom Teacher")
+            R.id.introverted_beauty_gets_raped_over_and_over_by_her_homeroom_teacher_2 -> manga.putExtra("name", "Introverted Beauty Gets Raped Over and Over by Her Homeroom Teacher 2")
+            R.id.kimi_no_namida_no_riyuu_o_ore_wa_mada_shiranai -> manga.putExtra("name", "Kimi no Namida no Riyuu o Ore wa Mada Shiranai")
         }
         startActivity(manga)
     }
